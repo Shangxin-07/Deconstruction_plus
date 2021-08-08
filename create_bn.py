7aaa9fb9f59a55e15334c74d57a4e7b9464122de
Shangxin-07 <86908160+Shangxin-07@users.noreply.github.com>
2021-08-08 13:45:00 +0800

    Added metal dec to reinforced; linted all files

--- create_bn.py
+++ create_bn.py
@@ -51,5 +51,8 @@ with open(bn_recipe_file, "w") as data_rfile:
 #print ( json.dumps(modinfo, indent=4) )
 #print ( json.dumps(groups, indent=4) )
 #print ( json.dumps(recipes, indent=4) )
+#find . -name "*.json" -print0 | xargs -0 -L 1 ./json_formatter.cgi
+#find . -name "*.BN" -print0 | xargs -0 -L 1 ./json_formatter.cgi
+
 
 quit()
